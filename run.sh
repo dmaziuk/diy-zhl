5563276ed2e981b9a344ef79922d2daa2b8cf585
Dimitri Maziuk <dmaziuk@bmrb.wisc.edu>
2019-09-12 14:42:00 -0500

    added gf blurb

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo docker run -it --rm --name=notebook --user root -e NB_UID=`id -u` -v `pwd`:/home/jovyan/work -p 8888:8888 jupyter/scipy-notebook
+sudo docker run -it --rm --name=notebook --user root -e NB_UID=`id -u` -v `pwd`:/home/jovyan/work/ -p 8888:8888 jupyter/scipy-notebook
